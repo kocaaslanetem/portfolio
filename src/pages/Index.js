188f861a3138928362e946a0fe7b9e42f5e356ba
Etem Kocaaslan <kaslanetem@gmail.com>
2026-06-25 01:16:50 +0300

    The typo is fixed and description is changed

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -5,7 +5,7 @@ import Main from '../layouts/Main';
 
 const Index = () => (
   <Main
-    description="Etem Kocaaslan's personal website. Turkiye based EEM Graduated from Eskisehir Technical University. "
+    description="Etem Kocaaslan's personal website. Electrical and Electronics Engineer with a passion for machine learning, robotics, and software development."
   >
     <article className="post" id="index">
       <header>
@@ -19,7 +19,7 @@ const Index = () => (
       <p>
         {' '}
         Welcome to my website. Please feel free to check out my{' '}
-        <Link to="/resume">resume</Link>, <Link to="/projects">projects</Link>,{' '}
+        <Link to="/resume">resume</Link>, <Link to="/projects">projects</Link>.{' '}
       </p>
     </article>
   </Main>
